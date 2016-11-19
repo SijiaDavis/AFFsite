d04a68c9fef247a9638ecac4c3f21fbaccd2da81
SijiaDavis <arwen@ksu.edu>
2016-11-19 12:02:44 -0500

    Add filter buttons

--- app.js
+++ app.js
@@ -10,12 +10,13 @@ $(document).ready(function() {
       
     }
 
-    $.get("https://api.myjson.com/bins/3nh96", function(data) {
+  
+      $.get("https://api.myjson.com/bins/3nh96", function(data) {
       
       $.each(data.items, function(i, item) {
         
           // append text-box div
-          $("<div>").addClass("text-box box"+i).appendTo("#col"+ i % 3);
+          $("<div>").addClass("text-box hidden box" + item.item_id + " " + item.service_name).appendTo("#col"+ i % 3);
           
           if (item.service_name === 'Manual') {
             // ribbon should display AFF
@@ -23,25 +24,25 @@ $(document).ready(function() {
               $("<div>").text(
                 "AFF"
               )  
-            ).appendTo("#col"+ i % 3 + " .box"+i);
+            ).appendTo("#col"+ i % 3 + " .box" + item.item_id);
             
-            $("<img>").attr("src", "assets/images/photo" + getRandomPicNum() + ".jpg" ).addClass("img-responsive center-block").appendTo("#col"+ i % 3 + " .box"+i);
+            $("<img>").attr("src", "assets/images/photo" + getRandomPicNum() + ".jpg").addClass("img-responsive center-block").appendTo("#col"+ i % 3 + " .box" + item.item_id);
             
-            $("<p>").text(item.item_data.text).appendTo("#col"+ i % 3 + " .box"+i);
-            $("<a>").text(item.item_data.link_text).attr({"href":item.item_data.link, "target": "new"}).appendTo("#col"+ i % 3 + " .box"+i);
+            $("<p>").text(item.item_data.text).appendTo("#col"+ i % 3 + " .box" + item.item_id);
+            $("<a>").text(item.item_data.link_text).attr({"href":item.item_data.link, "target": "new"}).appendTo("#col"+ i % 3 + " .box" + item.item_id);
           }
           else if (item.service_name === 'Twitter') {
-            $(".box"+i).addClass("box-twitter");
+            $(".box" + item.item_id).addClass("box-twitter");
             
             // ribbon should display twitter icon
             $("<div>").attr("id", "ribbon").addClass("ribbon-black").append(
               $("<div>").append(
                 $("<a>").addClass("fa fa-twitter fa-2x").attr("aria-hidden", "true")
               )  
-            ).appendTo("#col"+ i % 3 + " .box"+i);
+            ).appendTo("#col"+ i % 3 + " .box" + item.item_id);
             
-            $("<h3>").text(item.item_data.user.username).appendTo("#col"+ i % 3 + " .box"+i);
-            $("<h4>").text(item.item_data.tweet).appendTo("#col"+ i % 3 + " .box"+i);
+            $("<h3>").text(item.item_data.user.username).appendTo("#col"+ i % 3 + " .box" + item.item_id);
+            $("<h4>").text(item.item_data.tweet).appendTo("#col"+ i % 3 + " .box" + item.item_id);
             
           }
           else if (item.service_name === 'Instagram') {
@@ -50,18 +51,35 @@ $(document).ready(function() {
               $("<div>").append(
                 $("<a>").addClass("fa fa-instagram fa-2x").attr("aria-hidden", "true")
               )  
-            ).appendTo("#col"+ i % 3 + " .box"+i);
+            ).appendTo("#col"+ i % 3 + " .box" + item.item_id);
             
-            $("<img>").attr("src", "assets/images/photo" + getRandomPicNum() + ".jpg" ).addClass("img-responsive center-block").appendTo("#col"+ i % 3 + " .box"+i);
+            $("<img>").attr("src", "assets/images/photo" + getRandomPicNum() + ".jpg" ).addClass("img-responsive center-block").appendTo("#col"+ i % 3 + " .box" + item.item_id);
             
-            $("<h5>").text(item.item_data.user.username).appendTo("#col"+ i % 3 + " .box"+i);
-            $("<p>").text(item.item_data.caption).appendTo("#col"+ i % 3 + " .box"+i);
-          }
-        
-          if (i === 7) {
-             return false;
+            $("<h5>").text(item.item_data.user.username).appendTo("#col"+ i % 3 + " .box" + item.item_id);
+            $("<p>").text(item.item_data.caption).appendTo("#col"+ i % 3 + " .box" + item.item_id);
           }
        });
+    
+       function loadMore(){
+        if ($(".text-box.hidden").length < 9) {
+           console.log("running out");
+           
+        }
+        $("#col0 .text-box.hidden").slice(0,3).removeClass("hidden").addClass("displaying");
+        $("#col1 .text-box.hidden").slice(0,3).removeClass("hidden").addClass("displaying");
+        $("#col2 .text-box.hidden").slice(0,3).removeClass("hidden").addClass("displaying");
+       }
+       
+       loadMore();
+       $(".load-more-btn").on("click",loadMore);
+    
     });
+    
+    $(".nav-pills .btn").on("click", function(){
+      $(this).toggleClass("active");
+      var type = $(this).text();
+      $(".text-box.displaying."+ type).toggleClass("hidden");
+    });
+
 
 });
